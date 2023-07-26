c363d0a59e857298a24d5161226577e620043160
Hyan <hyanmarcos15@gmail.com>
2023-07-26 19:56:33 -0300

    config api

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,37 +1,72 @@
-import { Container, Form, Background } from './styles'
-import { Link } from 'react-router-dom'
-import { Input } from '../../components/input'
-import { FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { useState } from "react";
+import { Container, Form, Background } from "./styles";
+import { Link } from "react-router-dom";
+import { Input } from "../../components/input";
+import { FiUser, FiMail, FiLock } from "react-icons/fi";
 
-import { Button } from '../../components/Button'
+import { api } from "../../../../Backend/Api/src/services/api";
+
+import { Button } from "../../components/Button";
 
 export function SignUp() {
-    return(
-        <Container>
-            <Background/>
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-            <Form action="">
-                <h1>Rocket Notes</h1>
-                <p>Aplicação para salvar e grrenciar seus links úteis</p>
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos!");
+    }
 
-                <h2>Crie sua conta</h2>
+    api
+      .post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuário cadastrado com sucesso!");
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possível cadastrar");
+        }
+      });
+  }
 
-                <Input type="text" placeholder='Nome' icon={FiUser} />
+  return (
+    <Container>
+      <Background />
 
-                <Input type="text" placeholder='E-mail' icon={FiMail} />
+      <Form action="">
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e grrenciar seus links úteis</p>
 
-                <Input type="password" placeholder='Senha' icon={FiLock} />
+        <h2>Crie sua conta</h2>
 
-                <Button title='Cadastrar' />
+        <Input
+          type="text"
+          placeholder="Nome"
+          icon={FiUser}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-                <Link to='/'>
-                    Voltar para o login
-                </Link>
-            </Form>
+        <Input
+          type="text"
+          placeholder="E-mail"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-            
+        <Input
+          type="password"
+          placeholder="Senha"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        </Container>
+        <Button title="Cadastrar" onClick={handleSignUp} />
 
-    )
+        <Link to="/">Voltar para o login</Link>
+      </Form>
+    </Container>
+  );
 }
